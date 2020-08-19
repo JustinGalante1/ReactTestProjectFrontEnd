7fd5cfc5c6515dfc481bb49adad10a70b3b0327e
JustinGalante1 <jn2wf@virginia.edu>
2020-08-19 19:18:28 -0400

    first commit

---
+++ src/pages/home.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export class home extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+             
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Home page</h1>
+            </div>
+        )
+    }
+}
+
+export default home
---
+++ src/pages/login.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export class Login extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+             
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Login page</h1>
+            </div>
+        )
+    }
+}
+
+export default Login
